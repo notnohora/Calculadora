0ce844b4d1a9ee36854d55caf06a3b135afdde84
notnohora <nohora.cardozo@utp.edu.co>
2025-08-31 16:53:10 -0500

    intento de combinar % y () al operar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,6 +12,7 @@ import { last } from 'rxjs';
 })
 export class HomePage {
   constructor() {}
+  count = 0;
   parenthesis = 2;
   value = '';
   auxValue = '';
@@ -72,6 +73,8 @@ export class HomePage {
     }
   // Clear
     else if (Symbol === 'c'){
+      this.count = 0;
+      this.parenthesis = 2;
       this.expression = '';
       this.value = '';
       this.auxValue = '';
@@ -84,7 +87,7 @@ export class HomePage {
       if(!isNaN(Number(this.expression[this.expression.length-1]))){
         this.expression += ')';
       }
-      else if(['+', '-', '*', '/', '%', '('].includes(this.expression[this.expression.length-1]) ){
+      else if(['+', '-', '*', '/', '('].includes(this.expression[this.expression.length-1]) ){
         this.expression += '(';
       }
       else {
@@ -121,17 +124,68 @@ export class HomePage {
       }
     }
   // Equals
+  //QUEDE AQUI CUADRANDO COMO CALCULAR CON PARENTESIS Y PORCENTAJES JUNTOS 
+  // (100+20)% no funciona
     else if (Symbol === '='){
-      if(this.lastOperator !== '%' && this.value !== this.lastValue){
-        this.calculate(this.lastOperator);
+      this.count = this.expression.split('%').length - 1;
+        if(this.lastOperator !== '%' && this.value !== this.lastValue && this.count === 1){
+          this.calculate(this.lastOperator);
+        }
+        else if(this.count >= 1 && this.parenthesis > 2){
+
+//BORRADOR FALLIDOS
+          // try {
+          // let expr = this.expression;
+          // expr = expr.replace(/(\([^()]*\))%/, (match, p1) => {
+          //   return `(${p1}/100)`;  
+          // });
+
+
+    // this.value = eval(expr).toString();
+    // this.lastValue = this.value;
+  // } catch (e) {
+  //   this.value = 'Error';
+  // }
+        try {
+          let expr = this.expression;
+          // Reemplazar patrones tipo "A+B%" o "A-B%" por su versión expandida
+          expr = expr.replace(/(\d+(\.\d+)?)([+\-])(\d+(\.\d+)?)%/g, (match, num1, _, operator, num2) => {
+              return `${num1}${operator}(${num1}*${num2}/100)`;
+          });
+
+          // Reemplazar patrones tipo "A*B%" o "A/B%" por A*(B/100) o A/(B/100)
+          expr = expr.replace(/(\d+(\.\d+)?)([*\/])(\d+(\.\d+)?)%/g, (match, num1, _, operator, num2) => {
+              return operator === '*' 
+              ? `${num1}*(${num2}/100)` 
+              : `${num1}/(${num2}/100)`;
+          });
+          this.value = eval(expr).toString();
+          this.lastValue = this.value;
+          // this.expression = this.value;
+        } catch (e) {
+          this.value = 'Error';
+        }
       }
+      else if(this.count === 0 && this.parenthesis >= 2){
+        try {
+          this.value = eval(this.expression).toString();
+          this.lastValue = this.value;
+          // this.expression = this.value;
+        } catch (e) {
+          this.value = 'Error';
+        }
+        //
+      }
+      
     }
+  
   // Operators
     else if (['+', '-', '*', '/', '%'].includes(Symbol)){
       this.expression += Symbol;
       this.readyForNewInput = true;
       // Percent Cases
       if(Symbol === '%'){
+        if (this.parenthesis === 2){
         if(this.lastOperator === ''){
           this.lastValue = this.value
           this.value = '1'
@@ -148,6 +202,7 @@ export class HomePage {
         else if(this.lastOperator === '+' || this.lastOperator === '-'){
           this.calculate(Symbol)
         }
+      }
       }
       // else cases
       else{
